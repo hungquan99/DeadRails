cdfe90d34ec6c488b6987bfdffd2c809b9a8da76
LxckStxp <lxckstxp@gmail.com>
2025-03-03 08:34:31 +1000

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,12 +1,6 @@
--- Load modules
-local Config = loadstring(game:HttpGet("https://raw.githubusercontent.com/LxckStxp/DeadRails/main/modules/Config.lua"))()
-local Utilities = loadstring(game:HttpGet("https://raw.githubusercontent.com/LxckStxp/DeadRails/main/modules/Utilities.lua"))()
-local ESP = loadstring(game:HttpGet("https://raw.githubusercontent.com/LxckStxp/DeadRails/main/modules/ESP.lua"))()(Config, Utilities)
-local DensityManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/LxckStxp/DeadRails/main/modules/DensityManager.lua"))()(Config, Utilities, ESP)
-local TagFilter = loadstring(game:HttpGet("https://raw.githubusercontent.com/LxckStxp/DeadRails/main/modules/TagFilter.lua"))()(Config, Utilities, ESP)
+local Config = require(script:WaitForChild("modules/Config"))
+local Utilities = require(script:WaitForChild("modules/Utilities"))
+local ESP = require(script:WaitForChild("modules/ESP"))(Config, Utilities)
+local UI = require(script:WaitForChild("modules/UI"))(Config, ESP)
 
--- Initialize UI with all modules
-local UI = loadstring(game:HttpGet("https://raw.githubusercontent.com/LxckStxp/DeadRails/main/modules/UI.lua"))()(Config, ESP, TagFilter, DensityManager)
-
--- Initialize the ESP system
 ESP.Initialize()

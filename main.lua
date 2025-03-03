0c64be4151fa8a1ee5efd205e7634840fa8bf1f6
LxckStxp <lxckstxp@gmail.com>
2025-03-03 12:00:55 +1000

    Update main.lua

--- main.lua
+++ main.lua
@@ -8,7 +8,7 @@ local Utilities = loadstring(game:HttpGet(baseUrl .. "Utilities.lua"))()
 local ESP = loadstring(game:HttpGet(baseUrl .. "ESP/ESP.lua"))()(Config, Utilities)
 local MiddleClick = loadstring(game:HttpGet(baseUrl .. "MiddleClick.lua"))()
 local Aimbot = loadstring(game:HttpGet(baseUrl .. "Aimbot.lua"))()
-local UI = loadstring(game:HttpGet(baseUrl .. "UI.lua"))()(Config, ESP, MiddleClick)
+local UI = loadstring(game:HttpGet(baseUrl .. "UI.lua"))()(Config, ESP, MiddleClick, Aimbot)
 
 ESP.Initialize()
 MiddleClick.Initialize()

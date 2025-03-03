87d57e0e3df8d49721cf756e33f3930e9bf96367
LxckStxp <lxckstxp@gmail.com>
2025-03-03 11:52:26 +1000

    Update main.lua

--- main.lua
+++ main.lua
@@ -7,9 +7,11 @@ local Config = loadstring(game:HttpGet(baseUrl .. "Config.lua"))()
 local Utilities = loadstring(game:HttpGet(baseUrl .. "Utilities.lua"))()
 local ESP = loadstring(game:HttpGet(baseUrl .. "ESP/ESP.lua"))()(Config, Utilities)
 local MiddleClick = loadstring(game:HttpGet(baseUrl .. "MiddleClick.lua"))()
+local Aimbot = loadstring(game:HttpGet(baseUrl .. "Aimbot.lua"))()
 local UI = loadstring(game:HttpGet(baseUrl .. "UI.lua"))()(Config, ESP, MiddleClick)
 
 ESP.Initialize()
 MiddleClick.Initialize()
+Aimbot.Initialize()
 
 print("DeadRails ESP Loaded Successfully!")

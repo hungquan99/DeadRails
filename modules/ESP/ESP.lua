b183ea9458157871c7cb9fa3e5b48521e3b035da
LxckStxp <lxckstxp@gmail.com>
2025-03-03 10:11:41 +1000

    Update ESP.lua

--- modules/ESP/ESP.lua
+++ modules/ESP/ESP.lua
@@ -1,11 +1,10 @@
 return function(Config, Utilities)
-    local ESPConfig = loadstring(game:HttpGet("https://raw.githubusercontent.com/LxckStxp/DeadRails/main/modules/ESPConfig.lua"))()
-    local ESPObject = loadstring(game:HttpGet("https://raw.githubusercontent.com/LxckStxp/DeadRails/main/modules/ESPObject.lua"))()(Config, Utilities, ESPConfig)
-    local ESPManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/LxckStxp/DeadRails/main/modules/ESPManager.lua"))()(Config, Utilities, ESPObject, ESPConfig)
+    local ESPConfig = loadstring(game:HttpGet("https://raw.githubusercontent.com/LxckStxp/DeadRails/main/modules/ESP/ESPConfig.lua"))()
+    local ESPObject = loadstring(game:HttpGet("https://raw.githubusercontent.com/LxckStxp/DeadRails/main/modules/ESP/ESPObject.lua"))()(Config, Utilities, ESPConfig)
+    local ESPManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/LxckStxp/DeadRails/main/modules/ESP/ESPManager.lua"))()(Config, Utilities, ESPObject, ESPConfig)
     
     local ESP = {}
     
-    -- Expose public interface
     ESP.Initialize = ESPManager.Initialize
     ESP.Cleanup = ESPManager.Cleanup
     ESP.Update = ESPManager.Update
